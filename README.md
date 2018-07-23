a3540eba28194cb020a6fffbeee9cfc75430ac35
pwagner13 <pwagner@buffaloseminary.org>
2018-07-23 20:38:39 +0000

    Done.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 Your friend Harry Potter was very impressed with the invitations that you created for Ron Weasley's graduation party. Harry suggests that the two of you start an invitation printing business together. He wants you to create a program that prompts customers for the following party details...
 
+put = What is your name?
+guest_name = gets.chomp
+
+Put= 
 * guest_name
 * party_name
 * date
--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,44 @@
 # Code your prompts here!
 
+# * guest_name
+# * party_name
+# * date
+# * time
+# * host_name
+
+# ...and then prints out custom invitations that look something like this:
+
+# ```
+# Dear Hermione,
+
+# You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+
+# Sincerely,
+
+# Harry Potter
+# ```
+
+
 # Try starting out with puts'ing a string.
+ puts "Hi, you've been invited to a party! What is your name?"
+ guest_name = gets.chomp
+ 
+ puts "What is your party's name?"
+ party_name = gets.chomp
+ 
+ puts "When is your party?"
+ date = gets.chomp
+ 
+ puts "what time is your party"
+ time = gets.chomp
+ 
+ puts "What is the host's name?"
+ host_name = gets.chomp
+ 
+ 
+puts "Dear #{guest_name},
+
+You have been invited to #{party_name} on #{date} at #{time}.
+
+Sincerely,
+#{host_name}"
