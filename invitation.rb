3cf4ec5a45e142a574a7f89c3e232027b28d922b
pwagner13 <pwagner@buffaloseminary.org>
2018-07-23 20:23:48 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,6 +1,26 @@
 # Code your prompts here!
 
+# * guest_name
+# * party_name
+# * date
+# * time
+# * host_name
+
+# ...and then prints out custom invitations that look something like this:
+
+# ```
+# Dear Hermione,
+
+# You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+
+# Sincerely,
+
+# Harry Potter
+# ```
+
+
 # Try starting out with puts'ing a string.
- puts = What is your name?
+ puts "What is your name?"
+ 
  
 
